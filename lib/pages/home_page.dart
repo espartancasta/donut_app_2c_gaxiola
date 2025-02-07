052a75b0c521137fdad122555455d41c567d0280
JOSE CASTA <enriquecasta117@gmail.com>
2025-02-07 16:40:29 -0600

    FIRTS COMIT

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            //iconoizquierdo
+            leading: Icon(Icons.menu, color: Colors.grey[800]),
+            //icono derecho
+            actions: [Icon(Icons.person)]));
+  }
+}
